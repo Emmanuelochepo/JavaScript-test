14d79ae008b43160312ec77b080687d97d2607c9
Ochepo Emmanuel <ochepoemanuel@icloud.com>
2026-01-22 10:04:34 +0100

    Title: JavaScript Functions Practices Body: This is my JavaScript Functions Practics test from The Odin Project. In this little test i practiced how to Add, Multiply, Change strings Lowercase to Uppercase and how to slice out a letter from a string.

---
+++ script.js
@@ -0,0 +1,25 @@
+// Addition with Function
+function add7(num) {
+  alert(num + 7);
+}
+// add7(10);
+
+// Multiplication With Function
+function multiplyNo(num1, num2) {
+  alert(num1 * num2);
+}
+// multiplyNo(3, 2);
+
+// To Uppercase with Function.
+function toUppercase() {
+  let text = "emmanuel";
+  return text.toUpperCase();
+}
+// alert(toUppercase());
+
+// Extract last letter from string using a function
+function lastLetter() {
+  let letter = "Emmanuel";
+  return letter.slice(-1);
+}
+alert(lastLetter());
